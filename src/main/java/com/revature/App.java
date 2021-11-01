002fd9b224e6fb0b6053f1593bfff62e6e2dc0ee
Steven Hanley <Steven.Hanley@revature.net>
2021-11-01 02:07:46 -0500

    what

--- src/main/java/com/revature/App.java
+++ src/main/java/com/revature/App.java
@@ -1,31 +1,19 @@
 package com.revature;
 
 import com.revature.controllers.ReimbController;
-<<<<<<< HEAD
-=======
 import com.revature.controllers.UserController;
->>>>>>> 15b21f91cf350e808bf65bee87766e356963da0c
 import com.revature.controllers.Controller;
-
 import io.javalin.Javalin;
 import io.javalin.http.staticfiles.Location;
 
 public class App {
 	private static Javalin app;
 
-<<<<<<< HEAD
-	public static void main(String[] args) {
-		app = Javalin.create((config)->{
-			config.addStaticFiles("/static", Location.CLASSPATH);
-		});
-		configure(new ReimbController());
-=======
 	public static void main(String[] args) {	
 		app = Javalin.create((config)->{
 			config.addStaticFiles("/static", Location.CLASSPATH);
 		});
 		configure(new ReimbController(), new UserController());
->>>>>>> 15b21f91cf350e808bf65bee87766e356963da0c
 		app.start(8081);
 	}	
 	private static void configure(Controller... controllers) {
@@ -33,8 +21,4 @@ public class App {
 			c.addRoutes(app);
 		}
 	}
-<<<<<<< HEAD
-
-=======
->>>>>>> 15b21f91cf350e808bf65bee87766e356963da0c
 }
